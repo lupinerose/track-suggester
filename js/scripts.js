8d7fab862e4179de7653fbf78ea00029f4b05210
Brooke Kullberg <brookekullberg@gmail.com>
2019-03-29 15:33:58 -0700

    add css styling to index.html and add extra class to ensure form2 is hidden at begging of webpage load

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,9 @@
 $(document).ready(function(){
   $("#userInput").submit(function(event) {
-    $("#form2").show();
+    $("#form3").show();
     var userName = $("#name").val();
     $("#nameInsert").text(userName);
       $("#frontOrBackEnd").submit(function(event) {
-    // $("body").find(".col-md-4").addClass("hidden");
-    // $("body").find(".col-md-4").toggleClass("hidden");
         var devType = $("input:radio[name=devType]:checked").val();
         if (devType === "frontEnd") {
           $(location).attr('href', 'frontend.html')
