d564fe9eacec3c3fc3c6b8a41572a8ac8fe58a64
Brooke Kullberg <brookekullberg@gmail.com>
2019-03-29 13:43:18 -0700

    add javascript functions to show and hide areas of page based upon user input

--- js/scripts-backend.js
+++ js/scripts-backend.js
@@ -3,23 +3,38 @@ $(document).ready(function(){
     // $("body").find(".col-md-4").addClass("hidden");
     // $("body").find(".col-md-4").toggleClass("hidden");
     var backendType = $("input:radio[name=backendType]:checked").val();
-    $(answerArea).show();
+    $("#answerArea").show();
     if (backendType === "PHP") {
-      $("#answer").text("PHP");
-      // $("#snake, #salamander").hide();
+      $("#answer").text("PHP/Drupal");
+      $("#ruby, #java, #Csharp").hide();
+      $("#PHP").show();
     } else if (backendType === "ruby") {
-      $("#answer").text("Ruby");
-      // $("#turtle, #salamander").hide();
+      $("#answer").text("Ruby/Rails");
+      $("#PHP, #java, #Csharp").hide();
+      $("#ruby").show();
+
     } else if (backendType === "java") {
-      $("#answer").text("Java");
-      // $("#snake, #turtle").hide();
+      $("#answer").text("Java/Android");
+      $("#ruby, #PHP, #Csharp").hide();
+      $("#java").show();
+
     } else if (backendType === "Csharp") {
-      $("#answer").text("C#");
-      // $("#snake, #turtle").hide();
+      $("#answer").text("C#/.NET");
+      $("#ruby, #java, #PHP").hide();
+      $("#Csharp").show();
+
     } else if (backendType === "PHP") {
-      $("#answer").text("PHP");
-      // $("#snake, #turtle").hide();
+      $("#answer").text("PHP/Drupal");
+      $("#ruby, #java, #Csharp").hide();
+      $("#PHP").show();
+
+    } else if (backendType === "undecided") {
+      $("#answerArea2").show();
+      $("#undecidedAnswer").show();
+      $("#ruby, #java, #Csharp, #PHP").hide();
+      $("#answerArea").hide();
     }
+
 
     event.preventDefault();
 });
