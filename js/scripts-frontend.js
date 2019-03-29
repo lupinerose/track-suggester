80db3166e03ac38eb30c0ae8a2f1102383ee80f8
Brooke Kullberg <brookekullberg@gmail.com>
2019-03-29 11:31:56 -0700

    add JQuery fadeIn to show wells about more on CSS and more on Design

--- js/scripts-frontend.js
+++ js/scripts-frontend.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
   $("#btn-css").click(function(event) {
     $("#CSS").toggle();
+    $("#CSSinfo").fadeIn(3000);
     });
   $("#btn-design").click(function(event) {
     $("#design").toggle();
+    $("#designInfo").fadeIn(3000);
     });
 });
